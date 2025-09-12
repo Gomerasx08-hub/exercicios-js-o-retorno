3ff0a4902f900ad6873c6c8f7ad5d568e6ac6def
Gomerasx08-hub <beccasouzza08@gmail.com>
2025-09-11 22:17:44 -0300

    Exercicio 1 resolvido
    Resolução do exercicio 1

---
+++ exercicio001/app.js
@@ -0,0 +1,10 @@
+let nome = "", idade = 0;
+
+nome = prompt("Qual é o seu nome?");
+idade = prompt("Qual é a sua idade?");
+
+alert('\nOlá, seja bem vindo(a)! ' + nome + ', \nvoce tem ' + idade +' anos');
+
+document.write('\nOlá, seja bem vindo(a)! ' + nome + ', voce tem '
++ idade+ ' anos');
+
