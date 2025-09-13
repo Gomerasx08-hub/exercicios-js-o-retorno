edc28fb07bfb1c0f6da748f010672cd5efe0067f
Gomerasx08-hub <beccasouzza08@gmail.com>
2025-09-12 22:17:15 -0300

    exercicio003
    resolução do exercicio 003 concluida

---
+++ exercicio003/app.js
@@ -0,0 +1,14 @@
+function FnCalcularMedia(){
+    let vendedor = "", janeiro = 0, fevereiro = 0, marco = 0, media = 0
+    vendedor = document.getElementById("vendedor").value
+    alert("Seja bem vindo(a) " + vendedor)
+
+    janeiro = document.getElementById("janeiro").value
+    feveriro = document.getElementById("fevereiro").value
+    marco = document.getElementById("marco").value
+
+    media = (parseFloat(janeiro) + parseFloat(fevereiro) + parseFloat(marco)) / 3
+
+    document.getElementById("media").innerText = media.toFixed(2)
+
+}
