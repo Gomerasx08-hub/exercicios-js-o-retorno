8cab183cd045d964c2e1ca9ab2f6ee86e2771dd1
Gomerasx08-hub <beccasouzza08@gmail.com>
2025-09-26 21:58:27 -0300

    exercicio010
    resolução exercicio 010

--- exercicio009/app.js
+++ exercicio009/app.js
@@ -1,16 +1,20 @@
 function fnRepetirFrase(){
-    let qtd = 0, frase="", contador =0
-
+    let qtd = 0, frase = "", contador = 0
+ 
+    // ensinando a utilizar o while
+    // while(contador < 10){
+    //     console.log("olá " + contador)
+    //     contador = contador + 1                  ⬅️
+    //
+    //     contador++ (faz a mesma coisa do de cima ⬆️)
+    // }
+   
     frase = document.getElementById("frase").value
-    qtd = document.getElementById("qtd").value
-    document.getElementById("lista_frase").innerHTML = ""
-    while(contador < qtd) {
-        document.getElementById("lista_frase").
-        innerHTML += "<li>" + frase + "</li>"
+    qtd = parseInt(document.getElementById("qtd").value)
+    document.getElementById("lista_frases").innerHTML = ""
+ 
+    while(contador < qtd){
+        document.getElementById("lista_frases").innerHTML += "<li>" + frase + "</li>"
         contador++
     }
-    
-    
-
-
 }
